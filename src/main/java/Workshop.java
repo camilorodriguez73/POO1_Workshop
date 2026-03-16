11805ca9d8d779258092ace12d3eacd05a06b189
camilorodriguez73 <caarodriguezp@udistrital.edu.co>
2026-03-16 12:40:30 -0500

    Implementar esPrimo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -48,18 +48,20 @@ public class Workshop {
     }
 
 
-    // Método que verifica si un número es primo
+    // Método esprimo
     public boolean esPrimo(int numero) {
-        if (numero <= 1) {
+
+    if (numero <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i < numero; i++) {
+        if (numero % i == 0) {
             return false;
         }
+    }
 
-        for (int i = 2; i <= numero; i++) {
-            if(numero % i == 0){
-                return false;
-            }
-        }
-        return true;
+    return true;
     }
 
 
