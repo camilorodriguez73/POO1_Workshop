7afa5df096e7c4f8a13f27ec4f6000783c7c6140
camilorodriguez73 <caarodriguezp@udistrital.edu.co>
2026-03-16 12:52:05 -0500

    Implementar esPrimo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -121,10 +121,18 @@ public class Workshop {
 
     // Método que encuentra el elemento mayor en un arreglo
     public int encontrarElementoMayor(int[] arreglo) {
-        // TODO: Implementar el método para encontrar el elemento mayor en un arreglo.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5], el resultado debería ser 5.
-        return 0;
+
+    int mayor = arreglo[0];
+
+    for (int num : arreglo) {
+        if (num > mayor) {
+            mayor = num;
+        }
     }
+
+    return mayor;
+}
+
 
     // Método que encuentra el elemento menor en un arreglo
     public int encontrarElementoMenor(int[] arreglo) {
